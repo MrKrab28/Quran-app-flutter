887387d3b6058c57c70c70be962d030c543341fd
MrKrab28 <mr.krab2810@gmail.com>
2025-06-18 15:11:31 +0800

    add home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'package:quran_app/global.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text('Home Screen'),
-        ),
-      ),
+      backgroundColor: backgroundcolor,
+      appBar: _appBar(),
+      body: SafeArea(child: Center(child: Text('Home Screen'))),
     );
   }
+
+  AppBar _appBar() => AppBar(
+    title: Row(
+      children: [
+      Text('Quran App')
+    ]),
+  );
 }
